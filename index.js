e4bdaca471179817f06e03bc4e49d66e29e67502
Bryan Arias <brayank012@hotmail.com>
2022-08-11 23:06:42 -0500

    rout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { dbConection } = require('./database/config');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 // crear server
@@ -28,6 +29,10 @@ app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
   console.log('servidor corriendo en puerto ' + port);
